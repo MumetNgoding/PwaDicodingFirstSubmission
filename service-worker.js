d4f90b78ef97a33c2a6034c13faa9b8504c76e88
MumetNgoding <nugroho31.com@gmail.com>
2019-10-26 03:23:18 -0700

    Pwa Dicoding First Submission

---
+++ service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "firstpwa-3";
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/pages/home.html",
+  "/pages/about.html",
+  "/pages/privacy.html",
+  "/pages/contact.html",
+  "/css/materialize.min.css",
+  "/js/jquery.min.js",
+  "/js/materialize.min.js",
+  "/js/nav.js",
+  "/img/banner_1.jpg",
+  "/img/profile.jfif"
+];
+ 
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+//Menggunakan Asset Dari Cache
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then(function(response) {
+        if (response) {
+          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+          return response;
+        }
+ 
+        console.log(
+          "ServiceWorker: Memuat aset dari server: ",
+          event.request.url
+        );
+        return fetch(event.request);
+      })
+  );
+});
+
+//Mekanisme penghapusan Cache lama
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
